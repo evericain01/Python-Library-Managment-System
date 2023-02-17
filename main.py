af60cb1e9c5115a6e7493a6b3911f56f83490f09
Ebrahim Vericain <54519892+evericain01@users.noreply.github.com>
2023-02-17 21:41:49 +0000

    Changed some documentation

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+# This is a simple library management system to help librarians manage their libraries by allowing them to enter book information into the system. 
+# The application will consist of two classes: Book and Library. 
+# The Book class will store details about each book such as author, title, and publication year, while the Library class will manage the collection #and provide tools for adding, updating, and deleting books. 
+
+
 # Importing Random module to generate random ISBN numbers
 from random import randrange
 
